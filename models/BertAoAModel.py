57a7c1b8d8c2ee1b0719c6b2ae1e1329c22fa26d
userdyk-github <userdyk@gmail.com>
2020-07-07 20:16:53 +0000

    gram schmidt

--- models/BertAoAModel.py
+++ models/BertAoAModel.py
@@ -16,22 +16,6 @@ from .AttModel import pack_wrapper, AttModel, Attention
 from .TransformerModel import LayerNorm, attention, TransformerAttention, clones, SublayerConnection, PositionwiseFeedForward
 
 
-# grnet
-class GramSchmidt(nn.Module):
-
-    '''
-    def normalization(self, a):
-        return (1/math.sqrt((a*a).sum())) * a
-    '''
-
-    def projection(self, a,b):
-        return ((a*b).sum(dim=2, keepdim=True)/(b*b).sum(dim=2, keepdim=True)) * b
-
-    def forward(self, X):
-        for i in range(1, X.size(1)):
-            X[:,i:i+1,:].sub_(self.projection(X[:,i:i+1,:], X[:,0:i,:]).sum(dim=1, keepdim=True).detach())
-        X = X / X.norm(dim=2, keepdim=True).detach()
-        return X
 
 class MultiHeadedDotAttention(nn.Module):
     def __init__(self, h, d_model, dropout=0.1, scale=1, project_k_v=1, use_output_layer=1, do_aoa=0, norm_q=0, dropout_aoa=0.3, gsp=1):
@@ -214,6 +198,83 @@ class AoA_Decoder_Core(nn.Module):
 
         output = self.out_drop(output)
         return output, state
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# 2-dimensional
+class GramSchmidt02(nn.Module):
+    def projection(self, x, y):
+        z1 = torch.einsum('l,kl->k', [x,y])
+        z2 = torch.einsum('kl,kl->k', [y,y])
+        z = z1.div(z2)
+        return torch.einsum('k,kl->l', [z,y])
+
+    def forward(self, x):
+        for i in range(1, x.size(-2)):
+            x[i,:].sub_(self.projection(x[i,:], x[0:i,:]).detach())
+        z = torch.einsum('kl,kl->k', [x,x])
+        z = torch.div(torch.tensor(1.), torch.sqrt(z))
+        x = torch.einsum('k,kl->kl', [z,x])
+        #x = x / x.norm(dim=2, keepdim=True).detach()
+        return x
+
+
+# 3-dimensional
+class GramSchmidt03(nn.Module):
+    def projection(self, x, y):
+        z1 = torch.einsum('il,ikl->ik', [x,y])
+        z2 = torch.einsum('ikl,ikl->ik', [y,y])
+        z = z1.div(z2)
+        return torch.einsum('ik,ikl->il', [z,y])
+
+    def forward(self, x):
+        for i in range(1, x.size(-2)):
+            x[:,i,:].sub_(self.projection(x[:,i,:], x[:,0:i,:]).detach())
+        z = torch.einsum('ikl,ikl->ik', [x,x])
+        z = torch.div(torch.tensor(1.), torch.sqrt(z))
+        x = torch.einsum('ik,ikl->ikl', [z,x])
+        return x
+
+
+# 4-dimensional
+class GramSchmidt04(nn.Module):
+    def projection(self, x, y):
+        z1 = torch.einsum('ijl,ijkl->ijk', [x,y])
+        z2 = torch.einsum('ijkl,ijkl->ijk', [y,y])
+        z = z1.div(z2)
+        return torch.einsum('ijk,ijkl->ijl', [z,y])
+
+    def forward(self, x):
+        for i in range(1, x.size(-2)):
+            x[:,:,i,:].sub_(self.projection(x[:,:,i,:], x[:,:,0:i,:]).detach())
+        z = torch.einsum('ijkl,ijkl->ijk', [x,x])
+        z = torch.div(torch.tensor(1.), torch.sqrt(z))
+        x = torch.einsum('ijk,ijkl->ijkl', [z,x])
+        return x
+
+
+class GramSchmidt(nn.Module):
+    def __init__(self):
+        super(GramSchmidt, self).__init__()
+        self.gramschmidt02 = GramSchmidt02()
+        self.gramschmidt03 = GramSchmidt03()
+        self.gramschmidt04 = GramSchmidt04()
+
+    def forward(self, x):
+        dim = len(x.size());                             print(f'x : {x.size()}')
+        x = getattr(self, 'gramschmidt0'+str(dim))(x);   print(f'x : {x.size()}')
+        
+        # validation
+        for i in range(x.size(-2)):
+            if dim == 2   : print((x[0]*x[i]).sum())
+            elif dim == 3 : print((x[0][0]*x[0][i]).sum())
+            elif dim == 4 : print((x[0][0][0]*x[0][0][i]).sum())
+
+        return x
 
 
 
