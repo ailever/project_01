e0374153a075e4dd232a241952985bdb2e5fc3f9
userdky <userdyk@gmail.com>
2020-07-07 21:49:18 +0900

    stylize

---
+++ images/stylized_coco/models/download_models.sh
@@ -0,0 +1,2 @@
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=108uza-dsmwvbW2zv-G73jtVcMU_2Nb7Y' -O vgg_normalised.pth
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1w9r1NoYnn7tql1VYG3qDUzkbIks24RBQ' -O decoder.pth
